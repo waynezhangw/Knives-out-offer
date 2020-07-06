50ebc58366080a986da3754530da24ea64dedffc
WayneZhang <waynezhang226@gmail.com>
2020-07-06 23:11:35 +0800

    cut rope

---
+++ codes/55_cutRope.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using std::cout;
+class Solution {
+public:
+    int cutRope(int number) {
+        
+    }
+};
+
+int main() {
+	Solution sol;
+	cout<<sol.cutRope(8)<<std::endl;
+	return 0;
+}
